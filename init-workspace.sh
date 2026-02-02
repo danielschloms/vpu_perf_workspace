9e442fa8e02c00222dd3197f8d0641e137edd346
Daniel Schloms <daniel.schloms@gmail.com>
2026-02-02 15:01:12 +0000

    Update init script

--- init-workspace.sh
+++ init-workspace.sh
@@ -19,12 +19,9 @@ VICUNA_DIR=$PROJECT_ROOT_DIR/Vicuna2
 mkdir -p $PROJECT_ROOT_DIR/Third_Party
 mkdir -p $PROJECT_ROOT_DIR/Third_Party_Sources
 
+# Clone TUM repos and install Verilator + RISC-V GCC
 $SCRIPTS_DIR/clone-repos.sh
 $SCRIPTS_DIR/install-thirdparty.sh
-
-echo -e "${BLUE}Init Perfsim Submodules${NC}"
-cd $PERFSIM_DIR
-git submodule update --init --recursive
 
 echo -e "${BLUE}Init Vicuna 2.0 Submodules${NC}"
 cd $VICUNA_DIR
@@ -41,6 +38,29 @@ cd $VICUNA_DIR/rtl/fpu_ss
 git checkout $FPU_COMMIT
 cd $VICUNA_DIR/rtl/vproc_rtl
 git checkout $VPROC_COMMIT
+
+echo -e "${BLUE}Init Perfsim Submodules${NC}"
+cd $PERFSIM_DIR
+git submodule update --init --recursive
+
+echo -e "${BLUE}Check out known working Perfsim commits${NC}"
+M2_ISA_R_COMMIT="178cc2395a6155ec3955a83fbc2ba09586500f77"
+M2_ISA_R_PERF_COMMIT="b0336e9125230833566b8b9bfac6bcf09df40234"
+GENERATORS_DIR="$PERFSIM_DIR/code_gen/generators"
+M2_ISA_R_DIR="$GENERATORS_DIR/M2-ISA-R"
+M2_ISA_R_PERF_DIR="$GENERATORS_DIR/M2-ISA-R-Perf"
+
+cd $M2_ISA_R_PERF_DIR
+git checkout $M2_ISA_R_PERF_COMMIT
+
+cd $M2_ISA_R_DIR
+git checkout $M2_ISA_R_COMMIT
+
+echo -e "${BLUE}Replacing M2-ISA-R CoreDSL2 Parser${NC}"
+# TODO: could change .g4 grammar and rebuild it instead
+PARSER_FILE="$M2_ISA_R_DIR/m2isar/frontends/coredsl2/parser_gen/CoreDSL2Parser.py"
+rm $PARSER_FILE
+cp $SCRIPTS_DIR/CoreDSL2Parser.py $PARSER_FILE
 
 echo -e "${BLUE}Init Perfsim Workspace${NC}"
 $PERFSIM_DIR/scripts/setup_workspace.sh
