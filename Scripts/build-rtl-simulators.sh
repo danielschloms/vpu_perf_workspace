89035119607807bd57cc2d4776ca235d64ee60d1
Daniel Schloms <daniel.schloms@gmail.com>
2026-02-02 15:01:34 +0000

    Add script to build RTL simulators

--- Scripts/build-rtl-simulators.sh
+++ Scripts/build-rtl-simulators.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-########################
-# Build RTL simulators #
-########################
+###################################################################
+# Builds RTL simulators and simple example programs, runs them on #         
+# Verilator and ETISS, and performs an accuracy comparison.       #
+###################################################################
 
 # Terminal color
 MAGENTA='\033[0;35m'
@@ -10,15 +11,11 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-echo -e "${MAGENTA}Running simple tests${NC}"
+echo -e "${MAGENTA}Building RTL Simulators${NC}"
 
 SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
-VICUNA_DIR_NAME=Vicuna2
-VICUNA_DIR=$PROJECT_ROOT_DIR/$VICUNA_DIR_NAME
+TESTING_DIR=$PROJECT_ROOT_DIR/Perf_Comparison
 
-# VLANE_WIDTH must be <= VLEN / 2, at least 32, and a power of 2
-$VICUNA_DIR/build_rtl.sh --arch rv32im_zve32x --vlen 64 --vlane_width 32
-$VICUNA_DIR/build_rtl.sh --arch rv32im_zve32x --vlen 128 --vlane_width 32
-$VICUNA_DIR/build_rtl.sh --arch rv32im_zve32x --vlen 128 --vlane_width 64
-
+# Using --target dummy because either --target or --ctarget is currently required
+python3 $TESTING_DIR/testing/run-test-matrix.py --build_rtl --target dummy
