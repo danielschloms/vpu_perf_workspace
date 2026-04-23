8eb99f9405f97e095a766f90b985811e0200aab8
Daniel Schloms <daniel.schloms@gmail.com>
2026-04-23 10:58:19 +0000

    Create update branch

--- Scripts/update-softvector.sh
+++ Scripts/update-softvector.sh
@@ -6,17 +6,18 @@ PROJECT_ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
 ETISS_SV_DIR="$PROJECT_ROOT_DIR/Perfsim/etiss-perf-sim/etiss/build_dir/_deps/softvector_srcs-src"
 ETISS_SV_INCLUDE="$ETISS_SV_DIR/include/"
 ETISS_SV_SRC="$ETISS_SV_DIR/src/"
-ETISS_SV_MR="$ETISS_SV_DIR/mr-sv/"
+ETISS_SV_SF="$ETISS_SV_DIR/lib/softfloat-spike/"
 
 SV_DIR="$PROJECT_ROOT_DIR/Softvector"
 SV_INCLUDE="$SV_DIR/include/"
 SV_SRC="$SV_DIR/src/"
-SV_MR="$SV_DIR/mr-sv/"
+SV_SF="$SV_DIR/lib/softfloat-spike/"
 
 rsync -a $SV_INCLUDE $ETISS_SV_INCLUDE
 rsync -a $SV_SRC $ETISS_SV_SRC
-# rsync -a $SV_MR $ETISS_SV_MR
+rsync -a $SV_SF $ETISS_SV_SF
 
 cp $SV_DIR/CMakeLists.txt $ETISS_SV_DIR
+cp $SV_DIR/lib/CMakeLists.txt $ETISS_SV_DIR/lib/
 
 $PROJECT_ROOT_DIR/Perfsim/etiss-perf-sim/rebuild.sh
